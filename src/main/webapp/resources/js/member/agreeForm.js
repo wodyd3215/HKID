6303e5648a68241e73fd55ff9dc12c81f2ac2169
wodyd3215 <wodyd3215@naver.com>
2024-11-05 12:37:54 +0900

    update : 이용약관 UI JS 수정

--- src/main/webapp/resources/js/member/agreeForm.js
+++ src/main/webapp/resources/js/member/agreeForm.js
@@ -1,14 +1,16 @@
-
+window.onload = function() {
+    const checkBox = document.getElementById("all-check"); // 'all-check'라는 ID를 가진 체크박스를 가져옵니다.
+    allCheckBtn(checkBox); // 페이지 로드 시 체크박스 상태에 맞게 버튼 상태 초기화
+};
 
 function allCheckBtn(checkBox) {
     const enBtn = document.querySelector(".agreeContent button[type='submit']");
     enBtn.disabled = !checkBox.checked;
 
-    const allBtn = checkBox.checked;
     const checkList = document.getElementsByName("agreeCheck");
 
     for (const list of checkList) {
-        list.checked = allBtn;
+        list.checked = checkBox.checked;
     }
 
     for (const list of checkList) {
