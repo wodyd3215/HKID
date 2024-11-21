bfd3a8a2cbf7f8b7c0479433ac06a4b2ba0d9df5
hyerangbi <chococream_m@naver.com>
2024-11-21 11:52:18 +0900

    update - orderInfo

--- src/main/java/com/kh/hkid/phase/controller/PhaseInfoController.java
+++ src/main/java/com/kh/hkid/phase/controller/PhaseInfoController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,24 +27,36 @@ public class PhaseInfoController {
 	
 	@ResponseBody
 	@RequestMapping("phaseInfo.li")
-	public String selectPhaseInfo(HttpSession session, int productNo, Model model) {
+	public String selectPhaseInfo(HttpSession session, int productNo, Model model, Phase p) {
 		
 		int memberNo = ((Member)session.getAttribute("loginMember")).getMemberNo();
 		
 		if(memberNo == 0) {
 			model.addAttribute("errorMsg","로그인 이후 이용 가능한 서비스입니다.");
-		} 
+		}
 		
-		 
-		 HashMap<String, Object> order1= new HashMap<>();
-		 order1.put("memberNo", memberNo);
-		 order1.put("productNo", productNo);
-		 
-		 ArrayList<Phase> order = phaseService.getProduct(order1);
-		 
-		 model.addAttribute("order",order);
+		// 유저 정보와 상품 정보 불러오기
+//		model.addAttribute("m", phaseService.selectM(memberNo));
+//		model.addAttribute("p", phaseService.selectP(productNo));
 		
+		// 
+		
+		HashMap<String, Object> order1= new HashMap<>();
+		order1.put("memberNo", session.getAttribute("memberNo"));
+		order1.put("productNo", session.getAttribute("productNo"));
+		 
+		ArrayList<Phase> order = phaseService.getProduct(order1);		 
+		 
+		model.addAttribute("order",order);		 
 		
 		return "order/orderInfo";
+	}
+
+	
+	
+	@GetMapping("phase.in")
+	public String CompletePage(/*@RequestParam(value="") */HttpSession session, Phase p) {
+		session.setAttribute("",p);
+		return "order/orderComplete";
 	}
 }
--- src/main/java/com/kh/hkid/phase/service/PhaseService.java
+++ src/main/java/com/kh/hkid/phase/service/PhaseService.java
@@ -7,7 +7,10 @@ import com.kh.hkid.phase.model.vo.Phase;
 
 public interface PhaseService {
 
+
 	// 상품이랑 유저 정보 조회
 	ArrayList<Phase> getProduct(HashMap<String, Object> order1);
+
+	
 
 }
